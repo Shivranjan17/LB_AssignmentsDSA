0c4fc69702de92bd3fb19db61cd05368148e9d5a
“Shivranjan17” <shivranjanpathak792@gmail.com>
2024-05-21 01:07:49 +0530

    assignment 17 completed

---
+++ 17_program2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void Pattern(int iRow ,int iCol)
+{
+    int i = 0, j = 0;
+    for (i = 1; i <= iRow; i++)
+    {
+        for (j = 1; j <= iCol; j++)
+        {
+            if (j<=iCol-i +1)
+            {
+                printf("*\t");
+            }
+            
+
+
+        }
+        printf("\n");
+    }
+} 
+int main(int argc, char **argv)
+{
+    int iValue1 = 0; 
+    int iValue2 = 0;
+    printf("Enter Number of Rows and Column :");
+    scanf("%d%d",&iValue1,&iValue2);
+    Pattern(iValue1,iValue2);
+    return 0;
+}
